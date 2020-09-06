bc05ceb74052fb8d17244481e19794bb3a90d561
Villarreal <A127245@bf.arg.igrupobbva>
2020-09-06 20:29:48 -0300

    Commit

--- bk_servir_http.js
+++ bk_servir_http.js
@@ -2,14 +2,16 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const port = 3000
-const publicDirectory = path.join(__dirname, '../public/')
+//const port = 3000
+const port = process.env.PORT || 3000
+//const publicDirectory = path.join(__dirname, '../public/')
+const publicDirectory = path.join(__dirname, '/public/')
 
 app.use(express.static(publicDirectory))
 
-/*
+
 app.listen(port, () => {
     console.log('App running!')
 })
-*/
-app.listen(process.env.PORT || 3000);
+
+//app.listen(process.env.PORT || 3000);
